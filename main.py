9580184fc5db85738f65e62bd9663a16d9dbd7a0
Максим Ксенофонтов <ksenofontoffmaksim@list.ru>
2022-11-02 16:11:15 +0300

    Add Docstring for main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+"""Модуль позволяет создать персонажа и выполнять действия."""
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция отвечает за атаку персонажа.
+
+    Ключевые аргументы:
+    char_name -- имя персонажа
+    char_class -- класс персонажа
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +24,12 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция отвечает за защиту персонажа.
+
+    Ключевые аргументы:
+    char_name -- имя персонажа
+    char_class -- класс персонажа
+    """
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -25,6 +40,12 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция польволяет воспользоваться специальным умением.
+
+    Ключевые аргументы:
+    char_name -- имя персонажа
+    char_class -- класс персонажа
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f' «Выносливость {80 + 25}»')
@@ -36,6 +57,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Вызывается функция, введенная пользователем.
+
+    Ключевые аргументы:
+    char_name -- имя персонажа
+    char_class -- класс персонажа
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +87,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция позволяет выбрать класс персонажа.
+    Функция не принимает аргументов и возвращает строку."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -82,7 +111,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,6 +122,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
